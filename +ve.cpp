c8510e4fe070349035564f4dba4a37e71768ddac
Sneha Srivastava <srivastavasneha895@gmail.com>
2024-09-27 22:58:02 +0530

    added

---
+++ +ve.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+
+    if(n>0){
+        cout<<"it's a positive number"<<endl;
+    }
+    else{
+        cout<<"it's a negative number"<<endl;
+    }
+    return 0;
+}
