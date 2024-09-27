55b66088ce2f5d440c74572fe2eb1b022f177aed
Sneha Srivastava <srivastavasneha895@gmail.com>
2024-09-27 23:21:27 +0530

    added

---
+++ pattern1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,row,col;
+    cout<<"enter number of row"<<endl;
+    cin>>row;
+    cout<<"enter number of col"<<endl;
+    cin>>col;
+
+    for(i=1;i<=row;i++){
+        for(j=1;j<=col;j++){
+            cout<<"* ";
+        }
+        cout<<" ";
+    }
+    return 0;
+}
