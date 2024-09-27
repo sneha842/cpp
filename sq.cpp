c4bb9b7c6fd6d98c12fe6a0974becafb2ea2b19c
Sneha Srivastava <srivastavasneha895@gmail.com>
2024-09-27 21:37:04 +0530

    added

---
+++ sq.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,square,cube;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    square= n*n;
+    cube= n*n*n;
+
+    cout<<"square of the number"<<square<<endl;
+    cout<<"cube of the number"<<cube<<endl;
+    return 0;
+}
