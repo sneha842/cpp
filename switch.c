3b0284f65292ff75dc3bf5c5fbb5ec2bad373ec8
Sneha Srivastava <srivastavasneha895@gmail.com>
2024-10-01 20:31:27 +0530

    added

--- switch.c
+++ switch.c
@@ -12,36 +12,36 @@ int main(){
          cin>>a>>b;
          addition= a+b;
          cout<<"sum of a and b: "<<addition<<endl;
-         cin>>addition;
         
          break;
 
-        case 2:
+        case 2:{
          cout<<"enter two numbers"<<endl;
          cin>>a>>b;
          subtraction= a-b;
          cout<<"sub of a and b: "<<subtraction<<endl;
-          cin>>subtraction;
+         
+        }
         
          
          break;
 
-        case 3:
+        case 3:{
          cout<<"enter two numbers"<<endl;
          cin>>a>>b;
          multiplication= a*b;
          cout<<"multiplication of a and b: "<<multiplication<<endl;
-          cin>>multiplication;
-        
+          
+        }
          break;
 
-        case 4:
+        case 4:{
          cout<<"enter two numbers"<<endl;
          cin>>a>>b;
          division= a/b;
          cout<<"division of a and b: "<<division<<endl;
-          cin>>division;
-        
+          
+        }
          break;
 
         default:
