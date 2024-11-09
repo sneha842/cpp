ce8325aa96f40fc12ffe0ed7d2d0d59143a64432
Sneha Srivastava <srivastavasneha895@gmail.com>
2024-11-09 10:54:39 +0530

    added

--- max 3.cpp
+++ max 3.cpp
@@ -3,21 +3,18 @@ using namespace std;
 
 int main(){
     int a,b,c;
-    cout<<"enter 3 numbers"<<endl;
+    cout<<"enter three numbers"<<endl;
     cin>>a>>b>>c;
-     if(a>b){
-        if(a>c){
-            cout<<"a is greater"<<endl;
-            cin>>a;
-        }
-        else{
-            cout<<"c is greater"<<endl;
-            cin>>c;
-        }
-     }
-     else{
-        cout<<"b is graetaer"<<endl;
-        cin>>b;
-     }
-     return 0;
+    
+    if(a>b){
+        cout<<"a is greater"<<endl;
+    }
+    else if(a<b){
+        cout<<"b is greater"<<endl;
+    }
+    else{
+        cout<<"c is greater"<<endl;
+    }
+
+return 0;
 }
