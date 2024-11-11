573558a909f07865497998dd91a355535d9bbe26
Sneha Srivastava <srivastavasneha895@gmail.com>
2024-11-11 22:08:51 +0530

    added

---
+++ square.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string name= "Sneha";
+    // int n;
+    // cout<<"enter a number"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     cout<<name<<" ";
+    // }
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+         cout<<i*i<<" , ";
+    }
+    return 0;
+}
